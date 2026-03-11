4313525c46737907f0ad80e6c1e59da459b8a004
Jesper Tennevik <jesper.tennevik@gmail.com>
2026-03-11 01:31:17 +0100

    implemented scalable slider potentiometers and sends data as a percentage

--- src/main.c
+++ src/main.c
@@ -13,17 +13,18 @@
 
 #define NUM_BUTTONS sizeof(buttons) / sizeof(buttons[0])
 
+#define NUM_SLIDERS 2
+#define interval 100
+
 int main(void){
     millis_init();
     adc_init();
-    uart_init(9600);
+    uart_init(115200);
     sei();
-    // uint16_t slide_pot_0 = 0;
-    // uint16_t slide_pot_1 = 0;
+
+    uint16_t sliders[NUM_SLIDERS] = {0};
     millis_t now;
     millis_t last_tick = millis_get();
-    millis_t interval = 100;
-    // char send_buff[38];
 
     Button buttons[] = {
         { &DDRB, &PORTB, &PINB, PB4, 0, 0, 0 },
@@ -51,12 +52,18 @@ int main(void){
         }
 
         if(now - last_tick >= interval){
-            // sprintf(send_buff, "%u", slide_pot_0);
-            // uart_send_str_endl(send_buff);
-            // last_tick = now;
+            for(uint8_t i = 0; i < NUM_SLIDERS; i++){
+                char buff[16];
+                sprintf(buff, "Slider %u: %lu%%", i, ((uint32_t)sliders[i]*100)/1023);
+                uart_send_str_endl(buff);
+            }
+            last_tick = now;
         }
 
-        // slide_pot_0 = adc_read(0);
+        for(uint8_t i = 0; i < NUM_SLIDERS; i++){
+            sliders[i] = adc_read(i);
+        }
     }
+
     return 0;
 }
